63895633d1c34d49bbdcbcaf8d86c89475c2437a
Pradeep-Gopal <pradeepprachu@gmail.com>
2020-11-23 10:57:16 -0500

    [Updated license] Changed year in license

--- include/walker/walker.h
+++ include/walker/walker.h
@@ -8,7 +8,7 @@
 /**
  *  MIT License
  *
- *  Copyright (c) 2019 Pradeep Gopal
+ *  Copyright (c) 2020 Pradeep Gopal
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a
  *  copy of this software and associated documentation files (the "Software"),
